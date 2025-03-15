4b17f29ead73a2ec3ed1a3016b1cac0decfed854
Leon Oleschko <22222725+leoole100@users.noreply.github.com>
2025-03-15 23:31:34 +0100

    threading

--- software/main.py
+++ software/main.py
@@ -29,25 +29,37 @@ imshow(cam())
 # %%
 scale = 0.415
 f = None
-d = None
+dg = None
 p = 0.1
 threshold = 1e-200
 def loop():
-    global f, d, p 
+    global f, dg, p, pos
     f = cam()
     d = det(f)
+    tracking = False
     if d["signal"] > threshold:
+        tracking = True
         error = (d["center"]-np.array([0.5, 0.5]))*scale*[4/3, 1]
         pos[0] += p*error[0]
         m1(pos[0])
         pos[1] -= p*error[1] * - cos(pos[0])
         m2(pos[1])
+    dg = {
+        **d,
+        "tracking": tracking
+    }
+# measure_fps(loop) # 180Hz
 
-measure_fps(loop) # 180Hz
+#%%
+from threading import Thread
+running = True
+def tracking_task():
+    global running
+    while running:
+        loop()
 
-while True:
-    loop()
-    print(d)
+tracking = Thread(target=tracking_task)
+tracking.start()
 
 # %%
 # %%
