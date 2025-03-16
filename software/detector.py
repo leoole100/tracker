dbbcd48012f4ad730b3643c3726f6ed7913dcac6
Leon Oleschko <22222725+leoole100@users.noreply.github.com>
2025-03-16 12:55:01 +0100

    made some more changes

--- software/detector.py
+++ software/detector.py
@@ -2,15 +2,23 @@
 import numpy as np
 import numpy as np
 import cv2
+import math
+from glob import glob
+
+def log10_clamp(a):
+	if a<=0: return -math.inf
+	return math.log10(a)
 
 class Detector():
 	# size = (160, 120)
 	size = (80, 60)
 	def __init__(self):
-		template = cv2.imread("../ball.png")
-		template_lab = cv2.cvtColor(template, cv2.COLOR_BGR2HSV)
-		self.mean = np.mean(template_lab, axis=(0,1))
-		self.std = np.std(template_lab, axis=(0,1))
+		# templates = cv2.imread("../ball.png")
+		templates = [cv2.imread(p) for p in glob("../*.png")]
+		templates_lab = [cv2.cvtColor(i, cv2.COLOR_BGR2HSV) for i in templates]
+		lab = np.concatenate([t.reshape(t.shape[0]*t.shape[1], 3) for t in templates_lab])
+		self.mean = np.mean(lab, axis=0)
+		self.std = np.std(lab, axis=0)
 
 	# 3.6 ms
 	def __call__(self, f):
@@ -20,7 +28,10 @@ class Detector():
 		self.w = np.mean(self.diff**2 / self.std, axis=2)
 		self.w = np.exp(-self.w)
 		amax = np.unravel_index(self.w.argmax(), self.w.shape)
+		signal = log10_clamp(self.w[amax])*10
+		snr = log10_clamp(signal/np.mean(self.w))*10
 		return {
       		"center": (np.array(amax)/np.array(self.w.shape))[::-1].tolist(),
-			"signal": self.w[amax]
+			"signal": signal,
+			"snr": snr
 		}
